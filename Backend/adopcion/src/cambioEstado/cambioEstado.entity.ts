d3b5f952cce45588a3c22bc2ad29fdc04eac18b9
lu <lucianabertello8@gmail.com>
2025-08-26 14:22:22 -0300

    Entidades de ms-adopcion agregadas

---
+++ Backend/adopcion/src/cambioEstado/cambioEstado.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { Animal } from "src/animal/animal.entity";
+import { SolicitudAdopcion } from "src/solicitudAdopcion/solicitudAdopcion.entity";
+
+@Entity('cambio_estado')
+export class CambioEstado {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fechaHoraInicio: string;
+
+  @Column()
+  fechaHoraFin: string;
+
+  // Relaciones
+  
+  @ManyToOne(() => Animal, animal => animal.cambiosEstado)
+  animal: Animal;
+
+  @ManyToOne(() => SolicitudAdopcion, solicitud => solicitud.cambiosEstado)
+  solicitudAdopcion: SolicitudAdopcion;
+
+}
