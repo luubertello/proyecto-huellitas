f4fb362273afb669804a94fd795f336a54c1886f
celeste <celesteolmedo93@gmail.com>
2025-08-26 15:10:39 -0300

    Frontend - Página Inicio Sesión

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { IniciarSesion } from './pages/iniciar-sesion/iniciar-sesion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio-sesion', component: IniciarSesion },
+    {path: '', redirectTo: 'inicio-sesion', pathMatch: 'full'}
+];
--- Frontend/src/app/pages/iniciar-sesion/iniciar-sesion.ts
+++ Frontend/src/app/pages/iniciar-sesion/iniciar-sesion.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-iniciar-sesion',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './iniciar-sesion.html',
   styleUrl: './iniciar-sesion.css'
 })
-export class IniciarSesion {
+export class IniciarSesion implements OnInit {
+  inicioSesionClienteform!: FormGroup;
+  loading = false;
+  submitted = false;
+  showPassword = false;
+  errorMsg: string | null = null;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.inicioSesionClienteform = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+
+  }
 
 }
