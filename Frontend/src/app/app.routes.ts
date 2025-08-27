89fd9dd2257dd9ced651d19aefec4690afa9ba5d
celeste <celesteolmedo93@gmail.com>
2025-08-27 16:20:57 -0300

    Agregación de ruta de página Inicio

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { IniciarSesion } from './pages/iniciar-sesion/iniciar-sesion';
+import { Inicio } from './pages/inicio/inicio';
 
 export const routes: Routes = [
     { path: 'inicio-sesion', component: IniciarSesion },
+    { path: 'inicio', component: Inicio },
     {path: '', redirectTo: 'inicio-sesion', pathMatch: 'full'}
 ];
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './core/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
