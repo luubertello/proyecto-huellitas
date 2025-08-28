ab091af24ddc814e90f5493259f638894388d361
celeste <celesteolmedo93@gmail.com>
2025-08-28 12:23:10 -0300

    Creación página Registro

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { IniciarSesion } from './pages/iniciar-sesion/iniciar-sesion';
 import { Inicio } from './pages/inicio/inicio';
+import { Registro } from './pages/registro/registro';
 
 export const routes: Routes = [
     { path: 'inicio-sesion', component: IniciarSesion },
     { path: 'inicio', component: Inicio },
+    { path: 'registro', component: Registro },
     {path: '', redirectTo: 'inicio', pathMatch: 'full'}
 ];
