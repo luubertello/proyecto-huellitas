3affe60592ba7b03fcf9eb22cde083c7b2de7f2f
celeste <celesteolmedo93@gmail.com>
2025-09-03 14:37:36 -0300

    Actualización páginas Inicio Sesión y Registro

--- Frontend/src/app/pages/common/iniciar-sesion/iniciar-sesion.ts
+++ Frontend/src/app/pages/common/iniciar-sesion/iniciar-sesion.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit} from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-iniciar-sesion',
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './iniciar-sesion.html',
-  styleUrl: './iniciar-sesion.css'
+  styleUrl: './iniciar-sesion.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class IniciarSesion implements OnInit {
   inicioSesionClienteform!: FormGroup;
@@ -16,7 +18,39 @@ export class IniciarSesion implements OnInit {
   showPassword = false;
   errorMsg: string | null = null;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
+  
+  goRegistro() {
+      this.router.navigate(['/registro']);
+    }
+
+  goAdopcionGatos() {
+    this.router.navigate(['/adopcion/gatos']);
+  }
+
+  goAdopcionPerros() {
+    this.router.navigate(['/adopcion/perros']);
+  }
+
+  goAdopcionFormulario() {
+    this.router.navigate(['/adopcion/formulario']);
+  }
+
+  goAdopcionRequisitos() {
+    this.router.navigate(['/adopcion/requisitos']);
+  }
+
+  goAdopcion() {
+    this.router.navigate(['/adopcion']);
+  }
+
+  goDonar() {
+    this.router.navigate(['/donaciones']);
+  }
+
+  goInicio() {
+    this.router.navigate(['/inicio]']);
+  }
 
   ngOnInit(): void {
     this.inicioSesionClienteform = new FormGroup({
--- Frontend/src/app/pages/common/registro/registro.ts
+++ Frontend/src/app/pages/common/registro/registro.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
   templateUrl: './registro.html',
-  styleUrl: './registro.css'
+  styleUrl: './registro.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class Registro {
+  constructor (private router: Router) {}
+
+  goInicioSesion() {
+      this.router.navigate(['/inicio-sesion']);
+    }
+
+  goAdopcionGatos() {
+    this.router.navigate(['/adopcion/gatos']);
+  }
+
+  goAdopcionPerros() {
+    this.router.navigate(['/adopcion/perros']);
+  }
+
+  goAdopcionFormulario() {
+    this.router.navigate(['/adopcion/formulario']);
+  }
+
+  goAdopcionRequisitos() {
+    this.router.navigate(['/adopcion/requisitos']);
+  }
+
+  goAdopcion() {
+    this.router.navigate(['/adopcion']);
+  }
+
+  goDonar() {
+    this.router.navigate(['/donaciones']);
+  }
+
+  goInicio() {
+    this.router.navigate(['/inicio]']);
+  }
 
 }
