9ed56ac7f494ee79e4ce7398514177f0a90ca688
Milagros Cuello <milagroscuello2003@gmail.com>
2025-09-03 16:40:13 -0300

    Subido crear animal

--- Frontend/src/app/pages/admin/crear-animal/crear-animal.ts
+++ Frontend/src/app/pages/admin/crear-animal/crear-animal.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-crear-animal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './crear-animal.html',
-  styleUrl: './crear-animal.css'
+  styleUrls: ['./crear-animal.css']
 })
-export class CrearAnimal {
+export class CrearAnimal implements OnInit {
+  registrarAnimalForm!: FormGroup;
+  loading = false;
+  submitted = false;
+  errorMsg: string | null = null;
 
+  ngOnInit(): void {
+    this.registrarAnimalForm = new FormGroup({
+      nombre: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      especie: new FormControl('', [Validators.required]),
+      raza: new FormControl('', [Validators.required]),
+      edad: new FormControl('', [Validators.required, Validators.min(0)]),
+      sexo: new FormControl('', [Validators.required,Validators.pattern('^(Macho|Hembra)$')]),
+      descripcion: new FormControl(''),
+      imagen: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registrarAnimalForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    console.log('Datos del animal:', this.registrarAnimalForm.value);
+    // Aquí iría el POST al backend con HttpClient
+    this.loading = false;
+    
+  }
 }
