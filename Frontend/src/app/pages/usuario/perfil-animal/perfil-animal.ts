0feed41888a909c7303c1e4e23e5e60805e85fd4
celeste <celesteolmedo93@gmail.com>
2025-10-16 10:40:06 -0300

    Creación página Perfil-Animal

--- Frontend/src/app/pages/usuario/perfil-animal/perfil-animal.ts
+++ Frontend/src/app/pages/usuario/perfil-animal/perfil-animal.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-perfil-animal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './perfil-animal.html',
-  styleUrl: './perfil-animal.css'
+  styleUrl: './perfil-animal.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class PerfilAnimal {
+  animal: any;
 
-}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.registrarSolicitudAdopcion();
+  }
+
+  registrarSolicitudAdopcion(animal:any):void {
+    this.http.post('http://localhost:3000/adopta/formulario', { animalId: animal.id })
+      .subscribe(error => console.error('Error al registrar adopción.', error));
+}}
